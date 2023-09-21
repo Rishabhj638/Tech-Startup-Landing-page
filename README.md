411b4bd19304f4dabc110028856506061d6e7859
rishabh <rishabhj638@gmail.com>
2023-09-21 22:12:27 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+In this landing page we used TypeScript, JavaScript, TailWindCSS, HTML, ReactJS and Node JS.
---
+++ app/api/hello/route.ts
@@ -0,0 +1,3 @@
+export async function GET(request: Request) {
+  return new Response('This is the tech landing page')
+}
